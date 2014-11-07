b6455829184de4b5f406d33f023f9af8531ed9cc
kylim <thekoalaz@gmail.com>
2014-11-07 11:48:49 -0800

    Created basic maya command.

--- Maya/Main.cpp
+++ Maya/Main.cpp
@@ -1,8 +1,34 @@
-#include <maya/MSimple.h>
+#include <iostream>
+#include <maya/MString.h>
+#include <maya/MArgList.h>
+#include <maya/MFnPlugin.h>
+#include <maya/MPxCommand.h>
 #include <maya/MIOStream.h>
-DeclareSimpleCommand(helloWorld, "Autodesk", "2014");
-MStatus helloWorld::doIt(const MArgList&)
+class hello : public MPxCommand
 {
-	cout << "Hello World\n" << endl;
+public:
+	MStatus doIt(const MArgList& args);
+	static void* creator();
+};
+
+MStatus hello::doIt(const MArgList& args) {
+	cout << "Hello " << args.asString(0).asChar() << endl;
 	return MS::kSuccess;
 }
+
+void* hello::creator() {
+	return new hello;
+}
+
+MStatus initializePlugin(MObject obj) {
+	MFnPlugin plugin(obj, "Kevin Lim", "1.0", "Any");
+	plugin.registerCommand("hello", hello::creator);
+	return MS::kSuccess;
+}
+
+MStatus uninitializePlugin(MObject obj) {
+	MFnPlugin plugin(obj);
+	plugin.deregisterCommand("hello");
+	return MS::kSuccess;
+}
+
