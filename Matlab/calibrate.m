1b931b41856e576cc32d49fa6324bc6262abdbd2
bbinotti <bbinotti@gmail.com>
2014-12-03 13:04:53 -0800

    Obtains table depth if light depth is corrupt

--- Matlab/calibrate.m
+++ Matlab/calibrate.m
@@ -1,5 +1,5 @@
-function calibrate()
-[im, im_depth] = kinectframe();
+% function calibrate()
+% [im, im_depth] = kinectframe();
 
 %get search space window and write to .mat file
 figure, imshow(im);
@@ -9,7 +9,8 @@ save('data\search_space.mat', 'search_space');
 
 centroids = axisDetect(im, search_space);
 %append z
-centroids(1,3) = im_depth(round(centroids(1,2)), round(centroids(1,1)));
-centroids(2,3) = im_depth(round(centroids(2,2)), round(centroids(2,1)));
+% centroids(1,3) = im_depth(round(centroids(1,2)), round(centroids(1,1)));
+% centroids(2,3) = im_depth(round(centroids(2,2)), round(centroids(2,1)));
+centroids(:,3) = getDepth(im_depth, centroids);
 
 writeCoord(centroids);
