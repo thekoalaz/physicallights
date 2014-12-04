0c98984e66657cf57b04913defa6170db6cdb2ca
bbinotti <bbinotti@gmail.com>
2014-12-03 20:13:49 -0800

    Changed detection method

--- Matlab/axisDetect.m
+++ Matlab/axisDetect.m
@@ -1,8 +1,10 @@
-% function centroids = axisDetect(I_ir, ss)
+function centroids = axisDetect(I_ir, ss)
 
 %template as a cone
 template = zeros(30,30);
-template(11:21, 13:19) = ones(11,7);
+template(12:20, 14:18) = ones(9,5);
+templateS = imresize(template, .75);
+templateL = imresize(template, 1.5);
 
 %reduce search space to window assumption
 I = I_ir(ss(1,2) : ss(2,2), ss(1,1) : ss(2,1));
@@ -12,48 +14,31 @@ I = medfilt2(I);
 
 % optm_angle = optimalAxis(I, template);
 % disp(optm_angle);
-optm_angle = 0;
-template = imrotate(template, optm_angle);
-c = conv2(double(I), template);
-c = removeModel(c);
-% figure, imagesc(c);
-c = nonMaxSupr(c);
-% figure, imagesc(c);
+% optm_angle = 0;
+% template = imrotate(template, optm_angle);
+cL = conv2(double(I), templateL);
+% c = removeModel(c);
+% c = nonMaxSupr(c);
 
-peaks = c > (max(max(c))/5); %threshold;
-% peaks = bwmorph(peaks, 'skel', 10);
-% peaks = bwmorph(peaks, 'spur');
-% [x,y] = find(peaks == 1);
-% 
-% %adjust to conv size and cropped window
-% x = x - size(template,1)/2 + ss(1,2);
-% y = y - size(template,2)/2 + ss(1,1);
-% 
-% %find separate objects
-% deriv_filt = [-1; 1];
-% dx = conv(x, deriv_filt);
-% % dy = conv(y, deriv_filt);
-% temp = abs(dx(2:end-1)) > 5;
-% x1 = x(1 : find(temp));
-% x2 = x(find(temp) + 1 : end);
-% y1 = y(1 : find(temp));
-% y2 = y(find(temp) + 1 : end);
-% 
-% %keep center of objects
-% x1 = x1( ceil(size(x1,1)/2) );
-% x2 = x2( ceil(size(x2,1)/2) );
-% y1 = y1( ceil(size(y1,1)/2) );
-% y2 = y2( ceil(size(y2,1)/2) );
-% x = [x1, x2];
-% y = [y1, y2];
+% peaks = c > (max(max(c))/5); %threshold;
+peak1 = cL == (max(max(cL))); %axis1
+stats = regionprops(peak1, 'Centroid', 'Extrema');
+pos1 = stats.Centroid;
 
-stats = regionprops(peaks, 'Centroid', 'Extrema');
-c1 = stats(1).Centroid;
-c2 = stats(2).Centroid;
-centroids = [c1; c2];
+cS = conv2(double(I), templateS);
+cS = removeModel(cS, pos1);
+peak2 = cS == (max(max(cS))); %axis2
+stats = regionprops(peak2, 'Centroid', 'Extrema');
+pos2 = stats.Centroid;
+
+% stats = regionprops(peaks, 'Centroid', 'Extrema');
+% c1 = stats(1).Centroid;
+% c2 = stats(2).Centroid;
+
+centroids = [pos1; pos2];
+%pad it to have original image positions
 centroids(:,1) = centroids(:,1) + ss(1,1) - size(template,2)/2;
 centroids(:,2) = centroids(:,2) + ss(1,2) - size(template,1)/2;
-% stats(1).Extrema(1,:)
 
 figure, subplot(1,2,1), imshow(I_ir);
 hold on, plot(centroids(1,1), centroids(1,2), 'x', 'MarkerSize', 10);
--- Matlab/removeModel.m
+++ Matlab/removeModel.m
@@ -1,14 +1,18 @@
-function c_new = removeModel(c)
-c_m = size(c,1);
-c_n = size(c,2);
+function c_new = removeModel(c, position)
+% c_m = size(c,1);
+% c_n = size(c,2);
 
-center_m = round(c_m / 2);
-center_n = round(c_n / 2);
+% window_m = c_m/20;
+% window_n = c_n/20;
 
-window_m = round(c_m/40);
-window_n = round(c_n/40);
+window_m = 40;
+window_n = 40;
+
+min_m = max(0, position(2) - window_m/2)
+max_m = min(size(c,1), position(2) + window_m/2)
+min_n = max(0, position(1) - window_n/2)
+max_n = min(size(c,2), position(1) + window_n/2)
 
 c_new = c;
-c_new( center_m - window_m :  center_m + window_m , ...
-    center_n - window_n :  center_n + window_n ) = ...
-    zeros( window_m*2 + 1, window_n*2 + 1);
+c_new( min_m :  max_m , min_n :  max_n ) = ...
+    zeros( max_m - min_m + 1, max_n - min_n + 1 );
